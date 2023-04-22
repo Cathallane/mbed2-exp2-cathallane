ed35e9db8cb45e4bd1283f8d9fc6ed51f6bf9025
Cathallane <Lanecathal@gmail.com>
2023-04-22 10:49:51 +0000

    commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "mbed.h"
+#include"C12832.h"
+C12832 lcd(p5, p7, p6, p8, p11);
+PwmOut led1(LED1);
+AnalogIn pot1(p19);
+int main() {
+ while(1) {
+     lcd.printf("%2f",pot1.read());
+ led1 = pot1;
+ wait(0.01);
+ }
+}
